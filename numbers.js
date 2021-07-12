5359b0bafffd02403596e2cb095a49c7f5e662ce
TYLPHE <tylphet@gmail.com>
2021-07-11 22:06:41 -0600

    Playing with git and JavaScript

---
+++ numbers.js
@@ -0,0 +1,53 @@
+//number practice: I practice the content and then convert them into comments.
+
+// let x = 1;
+// x = -x;
+// alert(2**(1/2));
+
+// let s = "my" + "string";
+// alert(s);
+
+// let y="255";
+// alert(+y);
+// y = +y;
+// alert(typeof(y));
+
+// let a,b,c;
+// a = b = c = 2 + 2;
+// alert(a+b+c); 
+
+// let counter = 0;
+// counter++;
+// // ++counter;
+// alert(counter);
+
+// let counter = 1;
+// alert(2*++counter);
+// alert(2*counter++);
+
+// let a = (1+2,3+4);
+// alert(a);
+
+let a = 1, b = 1;
+let c = ++a;
+let d = b++;
+alert('Task 1: c = '+c+" and d = "+d);
+
+let e = 2;
+let x = 1+(e*=2);
+alert('Task 2: a = '+e+' and x = '+x);
+
+// alert(""+1+0);
+// alert(""-1+0);
+// alert(true+false);
+// alert(6/"3");
+// alert("2"*"3");
+// alert(4 + 5 + "px");
+// alert("$" + 4 + 5);
+// alert("4" - 2);
+// alert("4px" - 2);
+// alert("  -9  " + 5);
+// alert("  -9  " - 5);
+// alert(null + 1);
+// alert(undefined+1);
+// alert(" \t \n" - 2);
